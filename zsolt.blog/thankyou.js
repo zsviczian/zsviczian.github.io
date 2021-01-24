b422724bb7095c0943f2a87695d51b19e203ed16
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-01-24 17:49:01 +0100

    Update thankyou.js

--- zsolt.blog/thankyou.js
+++ zsolt.blog/thankyou.js
@@ -2,7 +2,7 @@ function thankyou() {
 	e = document.getElementsByClassName("MyFooter");	
 	let s = '<table><tbody><tr>';
 	s   += '<td><p style="margin-right: 24px;">Like this blog? <br/>Show your support.</p></td><td>';
-	s   += kofiwidget2.init("Buy me a coffee", "#eb6363", "D1D5302HF");kofiwidget2.draw();
+	//s   += kofiwidget2.init("Buy me a coffee", "#eb6363", "D1D5302HF");kofiwidget2.draw();
 	s   += '</td></tr></tbody></table>';
 	for (let i=0;i<e.length;i++) 
 		e[i].innerHTML = s;
