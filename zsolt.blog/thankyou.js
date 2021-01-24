0352dca44cefdabd4f9a5c58ea512b1887e220e6
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-01-24 17:28:30 +0100

    Update thankyou.js

--- zsolt.blog/thankyou.js
+++ zsolt.blog/thankyou.js
@@ -1,10 +1,10 @@
-      function thankyou() {
-        e = document.getElementsByClassName("MyFooter");
-		  let s = '<table><tbody><tr>';
-          s   += '<td><p style="margin-right: 24px;">Like this blog? <br/>Show your support.</p></td>';
-          s   += '<td><sc' + 'ript type="text/javascript" src="https://ko-fi.com/widgets/widget_2.js"><';
-          s   += '/script><sc'+'ript type="text/javascript">kofiwidget2.init("Buy me a coffee", "#eb6363", "D1D5302HF");kofiwidget2.draw();</';
-          s   += 'script></td></tr></tbody></table>';
-          e.innerHTML = s;
-          console.log(s);
-      }
+function thankyou() {
+	e = document.getElementsByClassName("MyFooter");	
+	let s = '<table><tbody><tr>';
+	s   += '<td><p style="margin-right: 24px;">Like this blog? <br/>Show your support.</p></td>';
+	s   += '<td><sc' + 'ript type="text/javascript" src="https://ko-fi.com/widgets/widget_2.js"><';
+	s   += '/script><sc'+'ript type="text/javascript">kofiwidget2.init("Buy me a coffee", "#eb6363", "D1D5302HF");kofiwidget2.draw();</';
+	s   += 'script></td></tr></tbody></table>';
+	for (let i=0;i<e.length;i++) 
+		e[i].innerHTML = s;
+}
